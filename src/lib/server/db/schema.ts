eaca5f70b607d01ca07efd455e59b55caf157797
rikiruswandii <rikiruswandi28@gmail.com>
2025-08-15 22:02:56 +0700

    feat:modified

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,20 +1,19 @@
-import { pgTable, serial, integer, text, timestamp } from 'drizzle-orm/pg-core';
+import { assignments } from "./schemas/assignment";
+import { courseModules, courses } from "./schemas/course";
+import { enrollments } from "./schemas/enrolement";
+import { lessonProgress, lessons } from "./schemas/lesson";
+import { sessions } from "./schemas/session";
+import { submissions } from "./schemas/submission";
+import { users } from "./schemas/user";
 
-export const user = pgTable('user', {
-	id: text('id').primaryKey(),
-	age: integer('age'),
-	username: text('username').notNull().unique(),
-	passwordHash: text('password_hash').notNull()
-});
-
-export const session = pgTable('session', {
-	id: text('id').primaryKey(),
-	userId: text('user_id')
-		.notNull()
-		.references(() => user.id),
-	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
-});
-
-export type Session = typeof session.$inferSelect;
-
-export type User = typeof user.$inferSelect;
+export const schemas = {
+	users,
+	sessions,
+	lessons,
+	lessonProgress,
+	courses,
+	courseModules,
+	assignments,
+	submissions,
+	enrollments,
+}
